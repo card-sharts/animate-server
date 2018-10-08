0435dbf418ddb4c01ac09fee20f0485ee2b906b5
rbtprograms <rbt61690@gmail.com>
2018-10-08 15:05:21 -0700

    post essay working not with photos

--- test/e2e/essays.test.js
+++ test/e2e/essays.test.js
@@ -11,6 +11,33 @@ describe('Essays API', () => {
 
     beforeEach(() => dropCollection('essays'));
 
+    const testEssay = {
+        title: 'My Wedding',
+        q1: 'My approach to wedding photography is to get wicked sick photos.',
+        q2: 'Context of this wedding is that these people love each other, which is wicked sick.',
+        q3: 'The biggest challenge was getting wicked sick photos.',
+        q4: 'My greatest success was getting some wicked sick photos.',
+        bangerUrl: 'https://res.cloudinary.com/dkbja8aak/image/upload/v1537564524/ajcjc8itv9z7rogs4r3j.jpg',
+        tags: ['black & white', 'same-sex'],
+        publishDate: '18-10-08'
+    };
+
+    let essay1;
+
+    beforeEach(() => {
+        return request
+            .post('/api/essays')
+            .send(testEssay)
+            .then(checkOk)
+            .then(({ body }) => {
+                essay1 = body;
+            });
+    });
+    
+    it('can post an essay', () => {
+        assert.isOk(essay1);
+    });
+
     it('gets an array on for a get all', () => {
         return request
             .get('/api/essays')
--- test/e2e/request.js
+++ test/e2e/request.js
@@ -1,12 +1,13 @@
-const { createServer } = require('http');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
+const http = require('http');
 
 const app = require('../../lib/app');
-const server = createServer(app);
+
+const server = http.createServer(app);
 const request = chai.request(server).keepOpen();
 
-after(done => server.close(done));
+after(() => server.close());
 
 module.exports = request;
