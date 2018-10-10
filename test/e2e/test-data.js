4e0335df81e78e060c0702b782c9668dbb0e57df
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-10-10 15:21:17 -0700

    Clean up essays test file and make test-data file

---
+++ test/e2e/test-data.js
@@ -0,0 +1,49 @@
+const testSubmission1 = {
+    title: 'My Wedding',
+    q1: 'My approach to wedding photography is to get wicked sick photos.',
+    q2: 'Context of this wedding is that these people love each other, which is wicked sick.',
+    q3: 'The biggest challenge was getting wicked sick photos.',
+    q4: 'My greatest success was getting some wicked sick photos.',
+    bangerUrl: 'https://res.cloudinary.com/dkbja8aak/image/upload/v1537564524/ajcjc8itv9z7rogs4r3j.jpg',
+    tags: ['black & white', 'same-sex'],
+    publishDate: '18-10-08',
+    photos: [
+        {
+            photoUrl: 'https://res.cloudinary.com/dkbja8aak/image/upload/v1537564524/ajcjc8itv9z7rogs4r3j.jpg'
+        }
+    ],
+    references: [
+        {
+            category: 'Florist',
+            websiteUrl: 'www.dopeflowers.com'
+        },
+        {
+            category: 'DJ',
+            websiteUrl: 'www.dopedj.com'
+        }
+    ]
+};
+
+const testSubmission2 = {
+    title: 'BEST WEDDING',
+    q1: 'I only shoot the best of the best.',
+    q2: 'Beautiful dresses make me happy.',
+    q3: 'Having to not drink at the open bar.',
+    q4: 'Every wedding is a great success.',
+    bangerUrl: 'https://res.cloudinary.com/dkbja8aak/image/upload/v1537564524/ajcjc8itv9z7rogs4r3j.jpg',
+    tags: ['jewish'],
+    publishDate: '18-10-12',
+    photos: [
+        {
+            photoUrl: 'https://res.cloudinary.com/dkbja8aak/image/upload/v1537564524/ajcjc8itv9z7rogs4r3j.jpg'
+        },
+        {
+            photoUrl: 'https://res.cloudinary.com/dkbja8aak/image/upload/v1537564524/ajrerr34v9z7rogs4r3j.jpg'
+        }
+    ]
+};
+
+module.exports = {
+    testSubmission1,
+    testSubmission2
+};
